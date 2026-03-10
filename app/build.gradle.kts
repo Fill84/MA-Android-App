41277f12178b9364a178a44e312784349b4f57ae
Fill84 <phil.pelzer@gmail.com>
2026-03-10 05:17:22 +0100

    feat: update versionCode and versionName to 5 and 3.0.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.musicassistant.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0.2"
+        versionCode = 5
+        versionName = "3.0.0"
     }
 
     signingConfigs {
